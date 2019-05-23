a97313a8f0704adfe3db9bb3cf2616d87ada857e
Sunny Heyar <sunny.heyar@gmail.com>
2019-05-23 19:34:58 -0400

    initial commit with directory structure

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./routes/controller");
+
+var app = express();
+
+var PORT = process.env.PORT || 3339;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
