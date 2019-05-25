c0a80a1d4ba3cf0638a9af3ecf80d098e5529f03
Orusa Ishtiaq <orusaishtiaq@gmail.com>
2019-05-25 10:38:51 -0400

    Created Database

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var routes = require("./routes/controller");
 var app = express();
 
 var PORT = process.env.PORT || 3339;
-
+var db = require("./models");
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +15,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(routes);
-
-app.listen(PORT, function() {
-  console.log(`Listening on http://localhost:${PORT}`);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log(`Listening on http://localhost:${PORT}`);
+  });
 });
