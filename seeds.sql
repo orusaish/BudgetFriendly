5d9de9714ee313f75d9b3ec0ffd213054f570231
Orusa Ishtiaq <orusaishtiaq@gmail.com>
2019-05-27 12:31:10 -0400

    added seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+DELETE FROM User WHERE id
+IN [1,2,3];
+INSERT INTO User
+    (id, name, email ,password)
+VALUES
+    (1, 'Mathew', "mathew@hotmail.com", "mathew123");
+INSERT INTO User
+    (id, name, email ,password)
+VALUES
+    (2, 'Sara', "saraM@hotmail.com", "sara987");
+INSERT INTO User
+    (id, name, email ,password)
+VALUES
+    (3, 'Mike', "kitty@gmail.com", "kitty654");
+    
+
