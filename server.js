05d56671ebf6cc46c29b1b38cc1a9802627fa667
Silent Dev <rez_a@outlook.com>
2019-05-28 19:37:44 -0400

    Fixed the icon logo on the navbar

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var routes = require("./routes/controller");
-
+var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 3339;
 var db = require("./models");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,8 +15,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(routes);
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log(`Listening on http://localhost:${PORT}`);
   });
 });
